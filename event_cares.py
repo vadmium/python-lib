2a959488b09b4df0f295596f80aae2b9a18179f1
Martin Panter <vadmium  gmail.com>
2011-07-04 02:46:20 +0000

    Resolver object with loop waiting on status called-back flag

--- event_cares.py
+++ event_cares.py
@@ -4,15 +4,55 @@ import cares
 import math
 
 class CaresResolver:
+    sock_notes = {
+        "r": QtCore.QSocketNotifier.Read,
+        "w": QtCore.QSocketNotifier.Write,
+    }
+    
     def __init__(self):
-        self.event = CaresQtEvent(self)
-        self.channel = cares.Channel(sock_state_cb=self.event.sock_state_cb)
+        self.channel = cares.Channel(sock_state_cb=self.sock_state_cb)
+        
+        self.timer = QtCore.QTimer(singleShot=True)
+        self.timer_event = QtSignalEvent(self.timer.timed_out)
+        
+        self.active_socks = dict((x, set(),) for x in sock_notes.keys())
+    
+    def sock_state_cb(self, s, read, write):
+        for (type, active,) in {"r": read, "w": write}.items():
+            if active:
+                self.socks[type].add(s)
+            else:
+                self.socks[type].discard(s)
+        
+        if not read and not write:
+            for x in self.socks.values():
+                x.pop(s, None)
+            return
     
     def resolve(self, name):
         self.status = None
+        
         self.channel.gethostbyname(name, 0, self.host_callback)
         while self.status is None:
-            yield self.event
+            events = event.EventSet()
+            
+            timeout = self.resolver.channel.timeout()
+            if timeout is not None:
+                self.timer.start(math.ceil(timeout * 1000))
+                events.add(self.timer.event, ("timer",))
+            
+            for (type, qt_type,) in self.sock_notes.items():
+                for sock in self.socks[type]:
+                    events.add(QtSocketEvent(sock, qt_type), ("sock", type,))
+            
+            ((id, *args,), result,) = (yield events)
+            
+            fds = dict.fromkeys(self.sock_notes.keys(), None)
+            if id == "sock":
+                self.timer.stop()
+                fds[args[0]] = result
+            self.resolver.channel.process_fd(fds["r"], fds["w"])
+        
         cares.check(self.status)
         raise StopIteration(
             (self.hostent["addrtype"], self.hostent["addr_list"][0],))
@@ -20,62 +60,3 @@ class CaresResolver:
     def host_callback(self, status, timeouts, hostent):
         self.status = status
         self.hostent = hostent
-
-class CaresEvent(event.Event):
-    note_types = {
-        "read": QtCore.QSocketNotifier.Read,
-        "write": QtCore.QSocketNotifier.Write,
-    }
-    
-    def __init__(self, resolver):
-        self.resolver = resolver
-        self.socks = dict((x, dict(),) for x in self.note_types.keys())
-        self.timer = QtCore.QTimer(singleShot=True, timeout=self.timed_out)
-    
-    def sock_state_cb(self, s, read, write):
-        if not read and not write:
-            for x in self.socks.values():
-                x.pop(s, None)
-            return
-        
-        for (type, active, callback,) in (
-            ("read", read, self.readable,),
-            ("write", write, self.writable,),
-        ):
-            socks = self.socks[type]
-            try:
-                socks[s].active = active
-            except KeyError:
-                if active:
-                    note = QtCore.QSocketNotifier(s, self.note_types[type],
-                        activated=callback)
-                    note.setEnabled(False)
-                    socks[s] = dict(note=note, active=True)
-    
-    def arm(self, callback):
-        self.client = callback
-        timeout = self.resolver.channel.timeout()
-        if timeout is not None:
-            self.timer.start(math.ceil(timeout * 1000))
-        self.set_armed(True)
-    
-    def readable(self, socket):
-        self.triggered((socket, None,))
-    def writable(self, socket):
-        self.triggered((None, socket,))
-    def timed_out(self):
-        self.triggered((None, None,))
-    
-    def triggered(self, cares_fds):
-        self.timer.stop()
-        self.set_armed(False)
-        
-        self.resolver.channel.process_fd(*cares_fds)
-        
-        self.client()
-    
-    def set_armed(self, enabled):
-        for socks in self.socks.values():
-            for sock in socks.values():
-                if sock["active"]:
-                    sock["note"].setEnabled(enabled)
