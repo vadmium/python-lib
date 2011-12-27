6b4df91cdb93997c9b237c61bbc82c71f02955da
Marty <->
2011-12-27 12:35:06 +1100

    Fuller function names, call hierarchy indentation
    Original point where “ftrace” module was separated

--- ftrace.py
+++ ftrace.py
@@ -8,12 +8,19 @@ class traced(Function):
     def __init__(self, func, name=None, abbrev=set()):
         self.func = func
         if name is None:
-            self.name = func.__name__
+            self.name = "{0.__module__}.{0.__name__}".format(func)
         else:
             self.name = name
         self.abbrev = abbrev
     
     def __call__(self, *args, **kw):
+        global startline
+        global indent
+        
+        if not startline:
+            print(file=stderr)
+        margin(stderr)
+        startline = False
         print(self.name, end="(", file=stderr)
         
         for (k, v) in enumerate(args):
@@ -26,29 +33,44 @@ class traced(Function):
             print(v, end="", file=stderr)
         
         comma = bool(args)
-        for (k, v) in kw:
+        for (k, v) in kw.items():
             if comma:
-                print(", ", end="", file=stderr)
+                stderr.write(", ")
             if k in self.abbrev:
                 v = "..."
             else:
                 v = repr(v)
-            print("{}={}".format(k, v), end="", file=stderr)
+            stderr.write("{0}={1}".format(k, v))
             comma = True
         
-        print(end=") ", file=stderr)
+        print(end=")", file=stderr)
+        indent += 1
         stderr.flush()
         ret = self.func(*args, **kw)
+        indent -= 1
+        
         if "return" in self.abbrev:
             v = "..."
         else:
             v = repr(ret)
+        if startline:
+            margin(stderr)
+        else:
+            stderr.write(" ")
         print("->", v, file=stderr)
+        startline = True
         return ret
 
 def trace(func, *args, **kw):
-    traced(func)(*args, **kw)
+    return traced(func)(*args, **kw)
 def tracer(name):
     return traced(nop, name=name, abbrev=set(("return",)))
 def nop(*args, **kw):
     pass
+
+indent = 0
+startline = True
+
+def margin(file):
+    for _ in range(indent):
+        file.write("  ")
