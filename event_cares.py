85cc02adbb177364d2c41cf526a5a003aa0b7d88
Marty <->
2012-01-12 11:08:33 +1100

    Address resolution independent of event system

--- event_cares.py
+++ event_cares.py
@@ -1,56 +1,55 @@
-from PyQt4 import QtCore
 import event
 import cares
-import math
 from socket import AF_UNSPEC
+from itertools import compress
 
-def resolve(name, family=AF_UNSPEC):
-    self = ResolveContext()
-    
-    self.timer = QtCore.QTimer(singleShot=True)
-    self.timer_event = QtSignalEvent(self.timer.timed_out)
+def resolve(event_driver, name, family=AF_UNSPEC):
+    self = ResolveContext(event_driver)
+    timer = event_driver.Timer(event.Callback())
     
     channel = cares.Channel(sock_state_cb=self.sock_state)
     
     channel.gethostbyname(name, family, self.host)
     while self.status is None:
-        events = event.Any()
+        events = event.Any(w.callback for w in self.watchers.values())
         
         timeout = channel.timeout()
         if timeout is not None:
-            timer.start(math.ceil(timeout * 1000))
-            events.add(timer.event)
-        
-        for op in self.sockops:
-            for sock in self.socks[op]:
-                events.add(QtSocketEvent(sock, qt_type))
+            timer.start(timeout)
+            events.add(timer.callback)
         
         (trigger, args) = (yield events)
         timer.stop()
         
-        fds = dict.fromkeys(self.sockops)
-        if isinstance(trigger, SocketEvent):
-            (fd,) = args
-            fds[trigger.note.type()] = fd
-        channel.process_fd(fds[self.read], fds[self.write])
+        if trigger is timer.callback:
+            channel.process_fd(*(None for op in self.ops))
+        else:
+            (fd, ops) = args
+            channel.process_fd(*
+                (fd if (op in ops) else None for op in self.ops))
     cares.check(self.status)
     raise StopIteration(self.hostent)
 
 class ResolveContext:
-    read = QtCore.QSocketNotifier.Read
-    write = QtCore.QSocketNotifier.Write
-    sockops = (read, write,)
-    
-    def __init__(self):
+    def __init__(self, event_driver):
+        self.event_driver = event_driver
         self.status = None
-        self.socks = dict((x, set()) for x in self.sockops)
+        self.watchers = dict()  # File watchers by file descriptor
+        self.ops = (self.event_driver.READ, self.event_driver.WRITE)
     
-    def sock_state(self, s, read, write):
-        for op in self.sockops:
-            if {self.read: read, self.write: write}[op]:
-                self.socks[op].add(s)
-            else:
-                self.socks[op].discard(s)
+    def sock_state(self, s, *ops):
+        if any(ops):
+            try:
+                watcher = self.watchers[s]
+            except LookupError:
+                watcher = self.event_driver.FileWatcher(s, event.Callback())
+                self.watchers[s] = watcher
+            watcher.watch(compress(self.ops, ops))
+        else:
+            try:
+                del self.watchers[s]
+            except LookupError:
+                pass
     
     def host(self, status, timeouts, hostent):
         self.status = status
