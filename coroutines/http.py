0cba2b62b36b55a76cdbb8448216c78eb2b5f0ff
Martin Panter <vadmium  gmailcom>
2012-02-10 12:33:09 +1100

    Making HTTP code reflect the built-in “http.client” module

--- coroutines/http.py
+++ coroutines/http.py
@@ -1,150 +1,189 @@
 # Reference: https://tools.ietf.org/html/rfc2616
 
-def response_handler(sock):
-    parser = Parser(sock)
-    yield parser.next_char()
+from http.client import (
+    HTTP_PORT, UnknownTransferEncoding, BadStatusLine, UnknownProtocol)
+import event
+import email.parser
+
+class HTTPConnection(object):
+    def __init__(self, sock):
+        self.sock = sock
+    def getresponse(self):
+        r = HTTPResponse(self.sock)
+        yield r.begin()
+        raise StopIteration(r)
+
+class HTTPResponse(object):
+    def __init__(self, sock):
+        self.sock = sock
+        self.size = None
     
-    class Http0_9(BaseException):
-        pass
-    
-    try:
-        # if response does not begin "HTTP/n.n 999 ", assume HTTP/0.9
-        # simple-response
-        
-        try:
-            pre_space = (yield parser.space())
-        except ExcessError as e:
-            raise Http0_9(e.data)
-        if parser.eol:
-            raise Http0_9(pre_space + parser.eol + parser.c)
-        
-        pattern = b"HTTP/"
-        for (i, p) in enumerate(pattern):
-            if i:
-                yield parser.next_char()
-            if not parser.c or ord(parser.c) != p:
-                raise Http0_9(pre_space + pattern[:i] + parser.c)
-        
-        major = bytearray()
-        for _ in range(parser.NUMBER_LIMIT):
-            yield parser.next_char()
-            if parser.c == b".":
-                break
-            major.extend(parser.c)
-            if not parser.c.isdigit():
-                raise Http0_9(pre_space + pattern + major)
+    def read(self, amt):
+        if self.size:
+            data = (yield self.sock.recv(min(self.size, amt)))
         else:
-            raise Http0_9(pre_space + pattern + major)
-        
-        minor = bytearray()
-        for _ in range(parser.NUMBER_LIMIT):
-            yield parser.next_char()
-            if not parser.c.isdigit():
-                break
-            minor.extend(parser.c)
-        else:
-            raise Http0_9(pre_space + pattern + major + b"." + minor)
-        
-        mid_space = bytearray()
-        for i in range(parser.TOKEN_LIMIT):
-            if i:
-                yield parser.next_char()
-            if not parser.c or parser.c.isspace():
-                break
-            mid_space.extend(parser.c)
-        try:
-            space = (yield parser.space())
-        except ExcessError as e:
-            raise Http0_9(pre_space + pattern + major + b"." + minor +
-                mid_space + e.data)
-        mid_space.extend(space)
-        if parser.eol:
-            raise Http0_9(pre_space + pattern + major + b"." + minor +
-                mid_space + parser.eol + parser.c)
-        
-        status = bytearray()
-        for i in range(3):
-            if i:
-                yield parser.next_char()
-            status.extend(parser.c)
-            if not parser.c.isdigit():
-                raise Http0_9(pre_space + pattern + major + b"." + minor +
-                    mid_space + status)
+            (self.size, data) = (yield self.chunks)
+        self.size -= len(data)
+        raise StopIteration(data)
     
-    except Http0_9 as e:
-        (pending,) = e.args
-        chunked = False
-    
-    else:
-        if int(major) != 1:
-            raise EnvironmentError("Unsupported: HTTP/{}".format(major))
-        
+    def begin(self):
+        parser = Parser(self.sock)
+        # else: parser.new_line()
         yield parser.next_char()
-        yield parser.space()
         
-        reason = bytearray()
-        for i in range(400):
-            if i:
+        try:
+            # if response does not begin "HTTP/n.n 999 ", assume HTTP/0.9
+            # simple-response
+            
+            try:
+                pre_space = (yield parser.space())
+            except ExcessError as e:
+                raise BadStatusLine(e.data)
+            if parser.eol:
+                raise BadStatusLine(pre_space + parser.eol + parser.c)
+            
+            pattern = b"HTTP/"
+            for (i, p) in enumerate(pattern):
+                if i:
+                    yield parser.next_char()
+                if not parser.c or ord(parser.c) != p:
+                    raise BadStatusLine(pre_space + pattern[:i] + parser.c)
+            
+            major = bytearray()
+            for _ in range(parser.NUMBER_LIMIT):
                 yield parser.next_char()
-                yield parser.after_eol()
-            if parser.eol is not None:
-                if not parser.at_lws():
+                if parser.c == b".":
                     break
-                else:
-                    reason.extend(parser.eol)
-            reason.extend(parser.c)
-        else:
-            raise ExcessError("Excessive status reason")
-        reason = reason.rstrip()
-        
-        chunked = (yield parser.headers())
-        
-        # pending = parser.c
-    
-    if not chunked:
-        raise NotImplementedError("Not chunked transfer encoding")
-    
-    for _ in range(30000):
-        yield parser.after_eol()
-        yield parser.space()
-        size = 0
-        if parser.eol is None:
-            for _ in range(30):
-                try:
-                    digit = int(parser.c, 16)
-                except ValueError:
-                    break
-                size = size * 16 + digit
-                yield parser.next_char()
+                major.extend(parser.c)
+                if not parser.c.isdigit():
+                    raise BadStatusLine(pre_space + pattern + major)
             else:
-                raise ExcessError("Excessive chunk size")
+                raise BadStatusLine(pre_space + pattern + major)
+            
+            minor = bytearray()
+            for _ in range(parser.NUMBER_LIMIT):
+                yield parser.next_char()
+                if not parser.c.isdigit():
+                    break
+                minor.extend(parser.c)
+            else:
+                raise BadStatusLine(pre_space + pattern + major + b"." +
+                    minor)
+            
+            mid_space = bytearray()
+            for i in range(parser.TOKEN_LIMIT):
+                if i:
+                    yield parser.next_char()
+                if not parser.c or parser.c.isspace():
+                    break
+                mid_space.extend(parser.c)
+            try:
+                space = (yield parser.space())
+            except ExcessError as e:
+                raise BadStatusLine(pre_space + pattern + major + b"." +
+                    minor + mid_space + e.data)
+            mid_space.extend(space)
+            if parser.eol:
+                raise BadStatusLine(pre_space + pattern + major + b"." +
+                    minor + mid_space + parser.eol + parser.c)
+            
+            status = bytearray()
+            for i in range(3):
+                if i:
+                    yield parser.next_char()
+                status.extend(parser.c)
+                if not parser.c.isdigit():
+                    raise BadStatusLine(pre_space + pattern + major + b"." +
+                        minor + mid_space + status)
+        
+        except BadStatusLine as e:
+            (pending,) = e.args
+            self.msg = message_from_string(b"")
+        
+        else:
+            if int(major) != 1:
+                raise UnknownProtocol("HTTP/{}".format(major))
+            
+            yield parser.next_char()
+            yield parser.space()
+            
+            reason = bytearray()
+            for i in range(400):
+                if i:
+                    yield parser.next_char()
+                    yield parser.after_eol()
+                if parser.eol is not None:
+                    if not parser.at_lws():
+                        break
+                    else:
+                        reason.extend(parser.eol)
+                reason.extend(parser.c)
+            else:
+                raise ExcessError("Excessive status reason")
+            reason = reason.rstrip()
+            
+            self.msg = (yield parser.headers())
+            
+            # pending = parser.c
+        
+        te = self.msg.get_all("Transfer-Encoding", [])
+        try:
+            (*split, last) = te[-1].rsplit(",", 1)
+            if last.lstrip() != "chunked":
+                raise ValueError()
+        except (LookupError, ValueError):
+            raise UnknownTransferEncoding("Not chunked transfer encoding")
+        
+        try:
+            (split,) = split
+        except ValueError:
+            te.pop()
+        else:
+            te[-1] = split.rstrip()
+        
+        del self.msg["Transfer-Encoding"]
+        for i in te:
+            self.msg["Transfer-Encoding"] = i
+        
+        self.chunks = self.Chunks(parser)
+    
+    def Chunks(self, parser):
+        for _ in range(30000):
             yield parser.after_eol()
-        yield parser.line()
-        
-        if not size:
-            break
-        
-        if size > 3000000:
-            raise ExcessError("Excessive chunk")
-        data = parser.c
-        from sys import stdout
-        while True:
-            stdout.buffer.write(data)
-            size -= len(data)
-            if size <= 0:
+            yield parser.space()
+            
+            size = 0
+            if parser.eol is None:
+                for _ in range(30):
+                    try:
+                        digit = int(parser.c, 16)
+                    except ValueError:
+                        break
+                    size = size * 16 + digit
+                    yield parser.next_char()
+                else:
+                    raise ExcessError("Excessive chunk size")
+                yield parser.after_eol()
+            
+            i = 0
+            while parser.eol is None:
+                i += 1
+                if i >= 3000:
+                    raise ExcessError("Excessive line")
+                yield self.next_char()
+                yield self.after_eol()
+            
+            if not size:
+                yield event.Yield((0, b""))
                 break
             
-            data = (yield sock.recv(min(size, 0x1000)))
-            if not data:
-                break
+            yield event.Yield((size, parser.c))
+            
+            yield parser.next_char()
+        else:
+            raise ExcessError("Excessive number of chunks")
         
-        yield parser.next_char()
-    else:
-        raise ExcessError("Excessive number of chunks")
-    
-    print("done")
-    yield parser.headers()
-    print("parsed trailers")
+        yield parser.headers()
 
 class Parser(object):
     def __init__(self, sock):
@@ -160,67 +199,20 @@ class Parser(object):
         Leaves with c = start of eol
         """
         
-        chunked = False
+        parser = email.parser.FeedParser()
         
-        #~ message = Message()
-        
-        for _ in range(300):
-            # End of headers if direct CRLF
-            if self.at_eol():
-                raise StopIteration(chunked)
-            
-            # read <token> :
-            # ws, chars except crlf and :, :, then rstrip token)
-            # If crlf before :, ignore the line
-            yield self.space()
-            
-            name = bytearray()
-            pattern = b"Transfer-Encoding".lower()
-            for p in pattern:
-                if (self.eol is not None or
-                not self.c or ord(self.c.lower()) != p):
-                    match = False
-                    break
-                yield self.next_char()
-                yield self.after_eol()
-            else:
-                match = True
-            for i in range(self.TOKEN_LIMIT):
-                if i:
-                    yield self.next_char()
-                    yield self.after_eol()
-                
-                if self.at_lws():
-                    pass
-                elif self.eol is not None or self.c == b":":
-                    break
+        self.eol = b""
+        for _ in range(30000):
+            if self.eol is not None:
+                if self.at_eol():
+                    raise StopIteration(parser.close())
                 else:
-                    match = False
-                
-                #~ if eol:
-                    #~ name.extend(eol)
-                #~ name.extend(c)
-            else:
-                raise ExcessError("Excessive token")
-            
-            if self.eol is None:
-                yield self.next_char()
-                yield self.space()
-            
-            if match:
-                pattern = b"chunked".lower()
-                for p in pattern:
-                    if (self.eol is not None or
-                    not self.c or ord(self.c.lower()) != p):
-                        match = False
-                        break
-                    yield self.next_char()
-                    yield self.after_eol()
-            substance = (yield self.line())
-            
-            chunked |= match and not substance
+                    parser.feed(self.eol.decode("Latin-1"))
+            parser.feed(self.c.decode("Latin-1"))
+            yield self.next_char()
+            yield self.after_eol()
         else:
-            raise ExcessError("Excessive number of headers")
+            raise ExcessError("Excessive headers")
     
     def space(self):
         """Read and skip over spaces
@@ -241,27 +233,6 @@ class Parser(object):
             space.extend(self.c)
         else:
             raise ExcessError("Excessive space", bytes(space))
-    
-    def line(self):
-        """Skip until the end of line is reached
-        
-        Entry after calling after_eol
-        """
-        
-        substance = False
-        for i in range(3000):
-            if i:
-                yield self.next_char()
-                yield self.after_eol()
-            
-            if self.at_lws():
-                pass
-            elif self.eol is not None:
-                raise StopIteration(substance)
-            else:
-                substance = True
-        else:
-            raise ExcessError("Excessive line")
 
     def after_eol(self):
         """eol, c -> (c, eol)
