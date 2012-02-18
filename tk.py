3b36d1b15ca68db4c0c7c7389ca13a2873c64840
Martin Panter <vadmium  gmailcom>
2012-02-18 12:38:01 +1100

    Create driver classes dynamically when given widget

--- tk.py
+++ tk.py
@@ -3,6 +3,7 @@ from functools import reduce
 from operator import or_
 from math import ceil
 from . import event
+from lib import Record
 import tkinter
 from tkinter.ttk import Label
 
@@ -11,51 +12,56 @@ def add_field(window, row, label, widget):
         row=row, column=0, sticky=tkinter.E + tkinter.W)
     widget.grid(row=row, column=1, sticky=tkinter.E + tkinter.W)
 
-class EventDriver(object):
+# Another potential API implementation
+if False:
+    from types import MethodType
+    class MethodClass(type):
+        def __get__(self, obj, cls):
+            if obj is None:
+                return self
+            return MethodType(self, obj)
+    class EventDriver(object):
+        def __init__(self, widget):
+            self.widget = widget
+        class FileEvent(..., metaclass=MethodClass):
+            def __init__(self, parent, *_, **__):
+                ...(parent.widget)
+            ...
+        ...
+
+def EventDriver(widget):
+    # Create subclasses with "widget" as a member that inherit from the
+    # driver classes defined below
+    return Record(
+        FileEvent=type("EventDriver.FileEvent", (FileEvent,), locals()),
+        Timer=type("EventDriver.Timer", (Timer,), locals()),
+    )
+
+class FileEvent(event.FileEvent):
     from tkinter import (READABLE as READ, WRITABLE as WRITE)
     
-    def __init__(self, tk):
-        self.tk = tk
-    
-    def FileEvent(self, *args, **kw):
-        return FileEvent(self.tk, *args, **kw)
-    
-    def Timer(self, *args, **kw):
-        return Timer(self.tk, *args, **kw)
-
-class FileEvent(event.Event):
-    def __init__(self, widget, fd):
-        self.tk = widget.tk
-        self.fd = fd
-        event.Event.__init__(self)
+    def __init__(self, *args, **kw):
+        self.tk = self.widget.tk
+        event.FileEvent.__init__(self, *args, **kw)
     
     def watch(self, ops):
         self.ops = reduce(or_, ops)
     
-    def writable(self):
-        self.ops = EventDriver.WRITE
-        return self
-    
-    def readable(self):
-        self.ops = EventDriver.READ
-        return self
-    
     def arm(self, *args, **kw):
-        event.Event.arm(self, *args, **kw)
+        event.FileEvent.arm(self, *args, **kw)
         self.tk.createfilehandler(self.fd, self.ops, self.handler)
     
     def close(self, *args, **kw):
         self.tk.deletefilehandler(self.fd)
-        event.Event.close(self, *args, **kw)
+        event.FileEvent.close(self, *args, **kw)
     
     def handler(self, fd, mask):
         if self.callback is not None:
             return self.callback((fd, set(op for op in
-                (EventDriver.READ, EventDriver.WRITE) if mask & op)))
+                (self.READ, self.WRITE) if mask & op)))
 
 class Timer(event.Event):
-    def __init__(self, widget):
-        self.widget = widget
+    def __init__(self):
         event.Event.__init__(self)
         self.timer = None
     
