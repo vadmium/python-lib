dfeada7168380bfb852406a25fb000a77eeda898
Martin Panter <vadmium  gmailcom>
2012-05-19 14:14:41 +1000

    “event” module moved into separate “lib” package

--- coroutines/http.py
+++ coroutines/http.py
@@ -2,7 +2,7 @@
 
 from http.client import (
     HTTP_PORT, UnknownTransferEncoding, BadStatusLine, UnknownProtocol)
-import event
+from lib import event
 import email.parser
 
 class HTTPConnection(object):
