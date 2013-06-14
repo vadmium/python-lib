c8b0f6df67360500c5cd95b44c11cb407b2d67d7
Martin Panter <vadmium  gmailcom>
2013-06-14 17:03:46 +1000

    PersistentConnectionHandler from python-iview

---
+++ iview/hds.py
@@ -0,0 +1,53 @@
+import urllib.request
+from http.client import HTTPConnection
+import http.client
+
+class PersistentConnectionHandler(urllib.request.BaseHandler):
+    def __init__(self, *pos, **kw):
+        self.type = None
+        self.host = None
+        self.pos = pos
+        self.kw = kw
+        self.connection = None
+    
+    def default_open(self, req):
+        if req.type != "http":
+            return None
+        
+        if req.type != self.type or req.host != self.host:
+            if self.connection:
+                self.connection.close()
+            self.connection = HTTPConnection(req.host, *self.pos, **self.kw)
+            self.type = req.type
+            self.host = req.host
+        
+        headers = dict(req.header_items())
+        try:
+            return self.open1(req, headers)
+        except http.client.BadStatusLine as err:
+            # If the server close the connection before receiving our reply,
+            # the "http.client" module raises an exception with the "line"
+            # attribute set to repr("")!
+            if err.line != repr(""):
+                raise
+        self.connection.close()
+        return self.open1(req)
+    
+    def open1(self, req, headers):
+        self.connection.request(req.get_method(), req.selector, req.data,
+            headers)
+        response = self.connection.getresponse()
+        
+        # Odd impedance mismatch between "http.client" and "urllib.request"
+        response.msg = response.reason
+        
+        return response
+    
+    def close(self):
+        if self.connection:
+            self.connection.close()
+    
+    def __enter__(self):
+        return self
+    def __exit__(self, *exc):
+        self.close()
