85c665ecd2e6044a40d2115fb881a64f4e2cd4a6
Martin Panter <vadmium  gmailcom>
2013-12-06 15:39:50 +1100

    Implement reader and writer callback API for TK event loop

--- coroutines/tk.py
+++ coroutines/tk.py
@@ -1,8 +1,7 @@
 from functools import reduce
-from operator import or_
+import operator
 from math import ceil
-from . import (FileEvent as BaseFileEvent, Event)
-from . import Send
+from . import Event
 from functions import weakmethod
 from warnings import warn
 from asyncio.base_events import BaseEventLoop
@@ -32,6 +31,7 @@ class EventLoop(BaseEventLoop):
     def __init__(self, widget):
         self.base_init()
         self._widget = widget
+        self._filehandlers = dict()
     
     def run_forever(self):
         self._widget.mainloop()
@@ -58,29 +58,36 @@ class EventLoop(BaseEventLoop):
         text.insert(tkinter.END, "\n".join(lines))
         text.see(tkinter.END)
         text["state"] = tkinter.DISABLED
-
-class FileEvent(BaseFileEvent):
-    READ = tkinter.READABLE
-    WRITE = tkinter.WRITABLE
     
-    def __init__(self, *args, **kw):
-        self.tk = self.widget.tk
-        BaseFileEvent.__init__(self, *args, **kw)
+    def add_reader(self, *pos, **kw):
+        return self._add_filehandler(tkinter.READABLE, *pos, **kw)
+    def add_writer(self, *pos, **kw):
+        return self._add_filehandler(tkinter.WRITABLE, *pos, **kw)
+    def _add_filehandler(self, mask, fd, callback, *pos, **kw):
+        callbacks = self._filehandlers.setdefault(fd, dict())
+        if callbacks:
+            self._widget.tk.deletefilehandler(fd)
+        callbacks[mask] = partial(callback, *pos, **kw)
+        mask = reduce(operator.or_, callbacks.keys())
+        self._widget.tk.createfilehandler(fd, mask, self._filehandler)
     
-    def watch(self, ops):
-        self.ops = reduce(or_, ops)
+    def remove_reader(self, *pos, **kw):
+        return self._remove_filehandler(tkinter.READABLE, *pos, **kw)
+    def remove_writer(self, *pos, **kw):
+        return self._remove_filehandler(tkinter.WRITABLE, *pos, **kw)
+    def _remove_filehandler(self, mask, fd):
+        callbacks = self._filehandlers[fd]
+        del callbacks[mask]
+        if not callbacks:
+            self._widget.tk.deletefilehandler(fd)
+            del self._filehandlers[fd]
     
-    def block(self, *args, **kw):
-        BaseFileEvent.block(self, *args, **kw)
-        self.tk.createfilehandler(self.fd, self.ops, self.handler)
-    
-    def close(self, *args, **kw):
-        self.tk.deletefilehandler(self.fd)
-        BaseFileEvent.close(self, *args, **kw)
-    
-    def handler(self, fd, mask):
-        trigger = set(op for op in (self.READ, self.WRITE) if mask & op)
-        return self.callback(Send((fd, trigger)))
+    def _filehandler(self, fd, mask):
+        self._widget.tk.deletefilehandler(fd)
+        callbacks = self._filehandlers.pop(fd)
+        for (cbmask, callback) in callbacks.items():
+            if mask & cbmask:
+                callback()
 
 class Timer(Event):
     def __init__(self):
