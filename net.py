9b81c8b5926b3ce6120461fba5f7d09dc42a2643
Martin Panter <vadmium  gmailcom>
2014-06-04 10:28:22 +0000

    Add header_list() function

--- net.py
+++ net.py
@@ -30,3 +30,28 @@ def formataddr(address):
     if not frozenset("[]:").isdisjoint(host):
         host = "[{}]".format(host)
     return "{}:{}".format(host, port)
+
+def header_list(message, header):
+    for header in message.get_all(header, ()):
+        sentinelled = header + ',"\\'
+        pos = 0
+        while pos < len(header):  # For each comma-delimited list element
+            start = pos
+            while True:  # For each quoted section within list element
+                comma = sentinelled.index(",", pos)
+                quote = sentinelled.index('"', pos)
+                if comma < quote:
+                    break
+                pos = quote + 1
+                while True:  # For each backslash escape in quote
+                    quote = sentinelled.index('"', pos)
+                    backslash = sentinelled.index("\\", pos)
+                    if quote < backslash:
+                        break
+                    pos = min(backslash + 2, len(header))
+                pos = min(quote + 1, len(header))
+            
+            elem = header[start:comma].strip()
+            if elem:
+                yield elem
+            pos = comma + 1
