3e369f6b415902d9a945ca5e107b3f636fe41ff6
Martin Panter <vadmium  gmailcom>
2014-08-06 11:35:03 +0000

    Use new Python 3.3 features

--- coroutines/socket.py
+++ coroutines/socket.py
@@ -1,7 +1,6 @@
 import socket
 import ssl
-from errno import (EINPROGRESS, EAGAIN)
-from ssl import (SSLError, SSL_ERROR_WANT_READ, SSL_ERROR_WANT_WRITE)
+from ssl import SSLWantReadError, SSLWantWriteError
 
 class Socket:
     """Provides coroutines for common blocking socket operations"""
@@ -16,20 +15,15 @@ class Socket:
             try:
                 self.sock.connect(*args, **kw)
                 break
-            except socket.error as err:
-                if err.errno != EINPROGRESS:
-                    raise
+            except BlockingIOError:
                 # Avoid yielding in exception handler
             yield self.event.writable()
     
     def recv(self, *args, **kw):
         while True:
             try:
-                raise StopIteration(self.sock.recv(*args, **kw))
-            except socket.error as err:
-                if err.errno != EAGAIN and (not isinstance(err, SSLError) or
-                err.args[0] != SSL_ERROR_WANT_READ):
-                    raise
+                return self.sock.recv(*args, **kw)
+            except (BlockingIOError, SSLWantReadError):
                 # Avoid yielding in exception handler
             yield self.event.readable()
     
@@ -46,20 +40,14 @@ class Ssl(Socket):
     def __init__(self, *args, **kw):
         Socket.__init__(self, *args, **kw)
         self.sock = ssl.wrap_socket(self.sock, do_handshake_on_connect=False)
-        
-        self.ssl_events = {
-            SSL_ERROR_WANT_READ: self.event.readable,
-            SSL_ERROR_WANT_WRITE: self.event.writable,
-        }
     
     def handshake(self, *args, **kw):
         while True:
             try:
                 self.sock.do_handshake(*args, **kw)
                 break
-            except SSLError as err:
-                if err.args[0] not in self.ssl_events:
-                    raise
-                # Avoid yielding in exception handler
-                event = self.ssl_events[err.args[0]]
+            except SSLWantReadError:
+                event = self.event.readable
+            except SSLWantWriteError:
+                event = self.event.writable
             yield event()
